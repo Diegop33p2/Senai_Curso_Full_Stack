2cb7e5c1d39a35e6d151fe0d291e87e680df5622
Diego Pereira <138408176+Diegop33p2@users.noreply.github.com>
2024-01-27 21:25:45 -0200

    js e css ajeitando login

--- site_games/js/jquery-script.js
+++ site_games/js/jquery-script.js
@@ -1,1 +1,11 @@
 //efeito de esconder formulário de cadastro
+
+$(document).ready(function(){
+    
+     $("#botao-cadastrar").click(function(){  //equivalente a: document.getElementById("botao_cadastrar").click
+        $("#form-cadastrar").slideToggle("slow");
+        $("#section-login").slideToggle("slow");//slideTogle esconde e amostra elemento. show/hide
+        $("#botao-cadastrar").hide();
+     });
+
+})
--- site_games/js/script.js
+++ site_games/js/script.js
@@ -1,5 +1,29 @@
 //efeito do botão voltar ao Topo
-
+function topo(){
+    window.scrollTo(
+        {
+            top:0 , //substitui o bottom e o right do id voltar-topo pelo left e right 0
+            left:0 ,
+            behavior: "smooth"
+        }
+    )
+}
 //Validação de Login
+function login(){
+    var logado = 0;
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
 
+    if(usuario == "diegop33" && senha == "12345678"){
+        window.location = "index.html";
+        logado = 1;
+    }
+    if(logado ==0){
+        alert("Login negado,Verifique o usúario e a senha e tente novamente.")
+    }
+}
 //Ativar alert no botão cadastrar
+function cadastro(){
+    alert("Cadastrado com sucesso!")
+    window.location.href = "index.html"
+}
